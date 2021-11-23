a0f0d5af423e8d57d13601f2611b81ea15b26ede
Blhammer <60705901+Blhammer@users.noreply.github.com>
2021-11-24 00:58:28 +0200

    Add files via upload

---
+++ 1. Programming Basics with C#/2. Simple Operations and Calculations/10. Commission/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Commission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int months = int.Parse(Console.ReadLine());
+            int percentageCommission = int.Parse(Console.ReadLine());
+            double pricePerMonth = double.Parse(Console.ReadLine());
+
+            int halfMonths = months / 2;
+            int otherMonths = months - halfMonths;
+
+            double monthPrice = ((halfMonths * pricePerMonth) + (otherMonths * pricePerMonth * 0.8));
+            double price = (monthPrice * percentageCommission / 100) + monthPrice;
+            Console.WriteLine($"Total: {price:f2}");
+        }
+    }
+}
