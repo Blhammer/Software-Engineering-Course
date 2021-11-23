130c96635b5215ca8a42f6d86c52de987a31274b
Blhammer <60705901+Blhammer@users.noreply.github.com>
2021-11-24 00:51:34 +0200

    Add files via upload

---
+++ 1. Programming Basics with C#/2. Simple Operations and Calculations/5. CompanyInfo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace companyInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameCompany = Console.ReadLine();
+            string address = Console.ReadLine();
+            int phoneNumberOffice = int.Parse(Console.ReadLine());
+            int fax = int.Parse(Console.ReadLine());
+            string website = Console.ReadLine();
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            int phoneNumberPersonal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The company name: {nameCompany}");
+            Console.WriteLine($"Address: {address}.");
+            Console.WriteLine($"Office phone number: {phoneNumberOffice}.");
+            Console.WriteLine($"Fax: {fax}.");
+            Console.WriteLine($"Website: {website}");
+            Console.WriteLine($"Manager - full name: {firstName} {lastName}");
+            Console.WriteLine($"Personal phone number: {phoneNumberPersonal}");
+        }
+    }
+}
