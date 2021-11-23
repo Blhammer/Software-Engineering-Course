dba9963e05751e18487b3a729c11d38f1f02f711
Blhammer <60705901+Blhammer@users.noreply.github.com>
2021-11-23 23:33:11 +0200

    Add files via upload

---
+++ 1. Programming Basics with C#/1. First Steps in Coding/4. PrintDateAndTime/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace printDateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime timeNow = DateTime.Now;
+
+            Console.WriteLine($"Current Date and time: {timeNow}");
+        }
+    }
+}
