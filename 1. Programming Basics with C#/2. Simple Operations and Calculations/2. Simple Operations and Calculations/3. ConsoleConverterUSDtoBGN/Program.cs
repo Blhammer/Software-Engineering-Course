e04d26d0bdf2070c7a247ef83a3bc5d3c286c026
Blhammer <60705901+Blhammer@users.noreply.github.com>
2021-11-24 00:50:10 +0200

    Add files via upload

---
+++ 1. Programming Basics with C#/2. Simple Operations and Calculations/2. Simple Operations and Calculations/3. ConsoleConverterUSDtoBGN/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace consoleConverterUSDtoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+
+            double bgn = usd * 1.69421;
+
+            Console.WriteLine($"{bgn:f2}");
+        }
+    }
+}
---
+++ 1. Programming Basics with C#/2. Simple Operations and Calculations/2. Simple Operations and Calculations/4. SimpleCalculations/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace simpleCalculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 123;
+
+            int a = number / 100;
+            int b = ((number / 10) % 10);
+            int c = number % 10;
+            
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
