fd5bd88a3b1767e54ddf0e05bda05e29a50d70e5
Blhammer <60705901+Blhammer@users.noreply.github.com>
2021-11-25 23:33:55 +0200

    Add files via upload

---
+++ 1. Programming Basics with C#/3. Conditional Statements/07. AreasOfDifferentGeomethicShapes/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _7._AreasOfDifferentGeomethicShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string shape = Console.ReadLine();
+
+            if (shape == "square")
+            {
+                double length = double.Parse(Console.ReadLine());
+
+                double area = length * length;
+
+                Console.WriteLine($"{area:f3}");
+            }
+            else if (shape == "rectangle")
+            {
+                double length = double.Parse(Console.ReadLine());
+                double width = double.Parse(Console.ReadLine());
+
+                double area = length * width;
+
+                Console.WriteLine($"{area:f3}");
+            }
+            else if (shape == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+
+                double area = Math.PI * radius * radius;
+
+                Console.WriteLine($"{area:f3}");
+            }
+            else if (shape == "triangle")
+            {
+                double length = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+
+                double area = (length * height) / 2;
+
+                Console.WriteLine($"{area:f3}");
+            }
+        }
+    }
+}
---
+++ 1. Programming Basics with C#/3. Conditional Statements/13. Add15Minutes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Add15Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int sumMinutes = hours * 60 + minutes;
+
+            int timePlusFifteen = sumMinutes + 15;
+
+            int finalhours = timePlusFifteen / 60;
+            int finalMinutes = timePlusFifteen % 60;
+
+            if (finalhours >= 24)
+            {
+                finalhours -= 24;
+            }
+
+            if (finalMinutes < 10)
+            {
+                Console.WriteLine($"{finalhours}:0{finalMinutes}");
+            }
+            else
+            {
+                Console.WriteLine($"{finalhours}:{finalMinutes}");
+            }
+        }
+    }
+}
