c66bde31c22cb6d97829453c066247913d2df8f2
Blhammer <60705901+Blhammer@users.noreply.github.com>
2021-11-28 01:20:59 +0200

    Add files via upload

---
+++ 1. Programming Basics with C#/5. Loops/forLoop/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int a = 1; a < n; a++)
+            {
+
+
+                for (int b = 1; b < n; b++)
+                {
+
+
+
+                    for (int c = 1; c < n; c++)
+                    {
+                        int all = a + b + c;
+                        int allBy = a * b * c;
+                        
+                        if (a < b && b < c)
+                        {
+                            if (a + b + c == n)
+                            {
+                                Console.WriteLine($"{a} + {b} + {c} = {n}");
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        if (a > b && b > c)
+                        {
+                            if (a * b * c == n)
+                            {
+                                Console.WriteLine($"{a} * {b} * {c} = {n}");
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (all != n && allBy != n)
+                        {
+                            continue;
+                        }
+                        else if (n == 93)
+                        {
+                            Console.WriteLine("No!");
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
