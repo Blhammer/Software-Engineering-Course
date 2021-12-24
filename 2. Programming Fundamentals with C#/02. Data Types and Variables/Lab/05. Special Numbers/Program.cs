921ecb22a13989491923b81a59400d7f04ad07d4
Blhammer <60705901+Blhammer@users.noreply.github.com>
2021-12-24 23:59:53 +0200

    Add files via upload

---
+++ 2. Programming Fundamentals with C#/02. Data Types and Variables/Lab/05. Special Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace _05._Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int num = 1; num <= n; num++)
+            {
+                int sum = 0;
+                int check = num;
+
+                while (check > 0)
+                {
+                    sum += check % 10;
+                    check /= 10;
+                }
+
+                bool isSpecial = (sum == 5) ||
+                    (sum == 7) ||
+                    (sum == 11);
+
+                Console.WriteLine($"{num} -> {isSpecial}");
+            }
+        }
+    }
+}
