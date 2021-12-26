0e74a033c74bb249bfca95c6d8a25dca06ae51bd
Blhammer <60705901+Blhammer@users.noreply.github.com>
2021-12-26 16:50:02 +0200

    Add files via upload

---
+++ 2. Programming Fundamentals with C#/02. Data Types and Variables/Lab/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _12._Refactor_Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int check = 0;
+           
+            for (int n = 1; n <= num; n++)
+            {
+                check = n;
+                while (n > 0)
+                {
+                    sum += n % 10;
+                    n = n / 10;
+                }
+
+                bool isValidNumber = (sum == 5) || 
+                    (sum == 7) || 
+                    (sum == 11);
+                
+                Console.WriteLine($"{check} -> {isValidNumber}");
+                sum = 0;
+                n = check;
+            }
+        }
+    }
+}
