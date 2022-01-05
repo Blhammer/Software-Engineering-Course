4cf5019ac3833e0ae8912b828f10f884ee3788e9
Blhammer <60705901+Blhammer@users.noreply.github.com>
2022-01-05 23:48:05 +0200

    Add files via upload

---
+++ 2. Programming Fundamentals with C#/03. Arrays/Exercise/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Zig_Zag_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            string[] firstArray = new string[numbers];
+            string[] secondArray = new string[numbers];
+
+            for (int i = 0; i < numbers; i++)
+            {
+                string[] currentArray = Console.ReadLine()
+                    .Split(" ")
+                    .ToArray();
+
+                string indexZero = currentArray[0];
+                string indexOne = currentArray[1];
+
+                if (i % 2 == 0)
+                {
+                    firstArray[i] = indexZero;
+                    secondArray[i] = indexOne;
+                }
+                else if (i % 2 == 1)
+                {
+                    firstArray[i] = indexOne;
+                    secondArray[i] = indexZero;
+                }
+            }
+            Console.WriteLine(string.Join(" ", firstArray));
+            Console.WriteLine(string.Join(" ", secondArray));
+        }
+    }
+}
