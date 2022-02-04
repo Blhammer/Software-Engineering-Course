748e075c563acc5e5cb436017be2020ff511ed6b
Blhammer <60705901+Blhammer@users.noreply.github.com>
2022-02-04 23:32:41 +0200

    Add files via upload

---
+++ 3. JavaScript/2. Advanced/Lab/05.Objects and Composition - Lab/01.CityRecord.js
@@ -0,0 +1,17 @@
+function solve(name, population, treasury) {
+    const record = {};
+
+    record.name = name;
+    record.population = population;
+    record.treasury = treasury;
+
+    return record;
+}
+solve('Tortuga',
+    7000,
+    15000
+);
+solve('Santo Domingo',
+    12000,
+    23500
+);
