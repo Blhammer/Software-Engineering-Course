77db40f52bb89b22efa66dc9a1ec619846b9d226
Blhammer <60705901+Blhammer@users.noreply.github.com>
2022-02-11 23:49:56 +0200

    Update script.js

--- 3. JavaScript/2. Advanced/Exercise/10.Advanced-DOM-Events - Exercise/01. Sections/script.js
+++ 3. JavaScript/2. Advanced/Exercise/10.Advanced-DOM-Events - Exercise/01. Sections/script.js
@@ -21,23 +21,3 @@ function create(words) {
         event.target.children[0].style.display = 'block';
     }
 }
-
-/* Event Delegation over parent
-let divs = [];
-   let divElement = document.getElementById('content');
-   for (const word of words) {
-      let div = document.createElement('div');
-      let p = document.createElement('p');
-      p.textContent = word;
-      p.setAttribute('style', 'display:none;');
-      div.appendChild(p)
-      divElement.appendChild(div);
-   }
- 
-   divElement.addEventListener('click', (e) => {
-      if (e.target.tagName === 'DIV' && !e.target.getAttribute('id')) {
-         let el = e.target.querySelector('p');
-         el.style.display = 'block';
-      }
-   });
-*/
