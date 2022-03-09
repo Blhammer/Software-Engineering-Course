d8eefdb07ba79f4741d8d286672aad2786f72902
Blhammer <60705901+Blhammer@users.noreply.github.com>
2022-03-09 23:44:51 +0200

    Add files via upload

---
+++ 3. JavaScript/2. Advanced/Exercise/16.Classes and Attributes - Exercise/03.Tickets.js
@@ -0,0 +1,35 @@
+function ticketSorter(tickets, sorting) {
+    class Ticket {
+        constructor(destination, price, status) {
+            this.destination = destination;
+            this.price = price;
+            this.status = status;
+        }
+    }
+
+    let result = [];//using the input array
+
+    tickets.map((el) => {
+        let [destination, price, status] = el.split('|');
+        price = Number(price);
+        result.push(new Ticket(destination, price, status));
+    });//foreach / map every element
+
+    return result.sort((a, b) => {
+        if (typeof a[sorting] === 'number') {
+            return a[sorting] - b[sorting];
+        } else {
+            return a[sorting].localeCompare(b[sorting]);
+        }
+    });
+}
+console.log(ticketSorter(['Philadelphia|94.20|available',
+    'New York City|95.99|available',
+    'New York City|95.99|sold',
+    'Boston|126.20|departed'],
+    'destination'));
+console.log(ticketSorter(['Philadelphia|94.20|available',
+    'New York City|95.99|available',
+    'New York City|95.99|sold',
+    'Boston|126.20|departed'],
+    'status'));
