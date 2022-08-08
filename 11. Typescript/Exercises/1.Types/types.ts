d99a5f800b8182a5cc912b33e4edd4d468f9a617
Blhammer <60705901+Blhammer@users.noreply.github.com>
2022-08-08 09:37:43 +0300

    Typescript course types

---
+++ 11. Typescript/Exercises/1.Types/types.ts
@@ -0,0 +1,35 @@
+let aged = true;
+let realAge = 0;
+
+let player = "rocky";
+
+let ball;
+
+let circleR: number;
+
+if (aged) {
+    realAge = 4;
+}
+
+var randomNumber = Math.random();
+if (randomNumber > 0.55) {
+    circleR = 5;
+} else {
+    circleR = 2;
+}
+
+let dogAge = realAge * 7;
+
+ball = "small";
+ball = false;
+ball = 35;
+
+console.log(`${dogAge} years`);
+
+console.log(player.toLowerCase());
+console.log(player.toUpperCase());
+console.log(player.length);
+
+console.log(ball);
+
+console.log(circleR);
