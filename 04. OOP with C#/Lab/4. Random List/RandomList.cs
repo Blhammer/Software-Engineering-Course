1b341c419751ae31b230b102c67ec3e246de61fa
Blhammer <60705901+Blhammer@users.noreply.github.com>
2022-09-07 22:40:05 +0300

    Added C# solution

---
+++ 04. OOP with C#/Lab/4. Random List/RandomList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random random = new Random();
+
+            var index = random.Next(0, Count);
+
+            string str = this[index];
+
+            this.RemoveAt(index);
+
+            return str;
+        }
+    }
+}
