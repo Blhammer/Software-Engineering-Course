092f6da9d8a7a572f0fd630ced7b55555bc04684
Blhammer <60705901+Blhammer@users.noreply.github.com>
2022-09-09 23:42:15 +0300

    Added solutions from the first exercise

---
+++ 12. Algorithms with C# Advanced/03.Bellman-Ford-and-Longest-Path-in-(DAG)-Exercise/1.Most-Reliable-Path/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+
+namespace Most_Reliable_Path
+{
+    public class Program
+    {
+        private const double Percentage = 100;
+
+        public static void Main()
+        {
+            int nodes = int.Parse(Console.ReadLine());
+            int edges = int.Parse(Console.ReadLine());
+
+            var graph = new List<Edge>[nodes];
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                graph[node] = new List<Edge>();
+            }
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                var first = edgeData[0];
+                var second = edgeData[1];
+                var weight = edgeData[2];
+
+                var edge = new Edge
+                {
+                    First = first,
+                    Second = second,
+                    Weight = weight
+                };
+
+                graph[first].Add(edge);
+                graph[second].Add(edge);
+            }
+
+            int source = int.Parse(Console.ReadLine());
+            int destination = int.Parse(Console.ReadLine());
+
+            double[] reliability = new double[graph.Length];
+            int[] prev = new int[graph.Length];
+
+            for (int node = 0; node < graph.Length; node++)
+            {
+                reliability[node] = double.NegativeInfinity;
+                prev[node] = -1;
+            }
+
+            reliability[source] = Percentage;
+
+            var bag = new OrderedBag<int>(
+                Comparer<int>.Create((f, s) =>
+                    reliability[s].CompareTo(reliability[f])));
+
+            bag.Add(source);
+
+            while (bag.Count > 0)
+            {
+                int node = bag.RemoveFirst();
+
+                if (node == destination)
+                {
+                    break;
+                }
+
+                foreach (var edge in graph[node])
+                {
+                    var child = edge.First == node
+                        ? edge.Second
+                        : edge.First;
+
+                    if (double.IsNegativeInfinity(reliability[child]))
+                    {
+                        bag.Add(child);
+                    }
+
+                    var newReliability = (reliability[node] * edge.Weight) / 100.0;
+
+                    if (newReliability > reliability[child])
+                    {
+                        reliability[child] = newReliability;
+                        prev[child] = node;
+
+                        bag = new OrderedBag<int>(
+                            bag,
+                            Comparer<int>.Create((f, s) =>
+                                reliability[s].CompareTo(reliability[f])));
+                    }
+                }
+            }
+
+            Console.WriteLine($"Most reliable path reliability: {reliability[destination]:F2}%");
+
+            var path = new Stack<int>();
+
+            var currentNode = destination;
+
+            while (currentNode != -1)
+            {
+                path.Push(currentNode);
+                currentNode = prev[currentNode];
+            }
+
+            Console.WriteLine(string.Join(" -> ", path));
+        }
+    }
+    public class Edge
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Weight { get; set; }
+    }
+}
---
+++ 12. Algorithms with C# Advanced/03.Bellman-Ford-and-Longest-Path-in-(DAG)-Exercise/2.Cheap-Town-Tour/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Cheap_Town_Tour
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var nodes = int.Parse(Console.ReadLine());
+            var edges = int.Parse(Console.ReadLine());
+
+            var graph = new List<Edge>();
+
+            for (int i = 0; i < edges; i++)
+            {
+                var edgeData = Console.ReadLine()
+                    .Split(" - ")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                graph.Add(new Edge
+                {
+                    First = edgeData[0],
+                    Second = edgeData[1],
+                    Weight = edgeData[2]
+                });
+            }
+
+            var parent = new int[nodes];
+            for (int node = 0; node < parent.Length; node++)
+            {
+                parent[node] = node;
+            }
+
+            var totalCost = 0;
+
+            foreach (var edge in graph.OrderBy(e => e.Weight))
+            {
+                var firstNodeRoot = FindRoot(edge.First, parent);
+                var secondNodeRoot = FindRoot(edge.Second, parent);
+
+                if (firstNodeRoot == secondNodeRoot)
+                {
+                    continue;
+                }
+
+                parent[firstNodeRoot] = secondNodeRoot;
+
+                totalCost += edge.Weight;
+            }
+
+            Console.WriteLine($"Total cost: {totalCost}");
+        }
+
+        private static int FindRoot(int node, int[] parent)
+        {
+            while (node != parent[node])
+            {
+                node = parent[node];
+            }
+
+            return node;
+        }
+    }
+
+    public class Edge
+    {
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Weight { get; set; }
+    }
+}
