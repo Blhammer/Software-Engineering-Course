73d718d7b3e16628f30e7b9a78c26fe56b4e8233
Blhammer <60705901+Blhammer@users.noreply.github.com>
2022-10-07 22:01:51 +0300

    Added a solution to the second preparation to the C# algorithms course

---
+++ 12. Algorithms with C# Advanced/09.Exam-Preparation-2/3.Road-Trip/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Road_Trip
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var values = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            var weights = Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+
+            var maxCapacity = int.Parse(Console.ReadLine());
+
+            var table = new int[values.Length + 1, maxCapacity + 1];
+
+            for (int row = 1; row < table.GetLength(0); row++)
+            {
+                var itemValue = values[row - 1];
+                var itemWeight = weights[row - 1];
+
+                for (int capacity = 0; capacity < table.GetLength(1); capacity++)
+                {
+                    var skipItemValue = table[row - 1, capacity];
+
+                    if (itemWeight > capacity)
+                    {
+                        table[row, capacity] = skipItemValue;
+                        continue;
+                    }
+
+                    var includeItemValue = itemValue + table[row - 1, capacity - itemWeight];
+
+                    table[row, capacity] = Math.Max(skipItemValue, includeItemValue);
+                }
+            }
+
+            Console.WriteLine($"Maximum value: {table[values.Length, maxCapacity]}");
+        }
+    }
+}
