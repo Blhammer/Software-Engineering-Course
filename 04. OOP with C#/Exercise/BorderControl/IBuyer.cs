858b7ba23082602576fc8c1f01ddf7eeb2be1c78
Blhammer <emo.2012012@gmail.com>
2022-11-20 23:59:57 +0200

    added a c# oop solution

---
+++ 04. OOP with C#/Exercise/BorderControl/IBuyer.cs
@@ -0,0 +1,9 @@
+﻿namespace BorderControl
+{
+    public interface IBuyer // 6. Food Shortage
+    {
+        int Food { get; }
+        
+        void BuyFood();
+    }
+}
---
+++ 04. OOP with C#/Exercise/BorderControl/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection.Metadata;
+
+namespace BorderControl
+{
+    public class Pet : ICelebratable
+    {
+        // 5.Birthday Celebrations
+        public Pet(string name, string birthdate)
+        {
+            Name = name;
+            Birthdate = birthdate;
+        }
+
+        public string Name { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
