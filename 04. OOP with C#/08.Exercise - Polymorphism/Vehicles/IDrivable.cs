a4119460eb9801cb418f749e6691daa4075f3e20
Blhammer <emo.2012012@gmail.com>
2022-12-02 19:28:11 +0200

    Add solution to C# OOP course

---
+++ 04. OOP with C#/08.Exercise - Polymorphism/Vehicles/IDrivable.cs
@@ -0,0 +1,10 @@
+﻿namespace Vehicles
+{
+    public interface IDrivable
+    {
+        double FuelQuantity { get; }
+        double FuelConsumptionPerKm { get; }
+        void Drive(double distance);
+        void Refuel(double amountFuel);
+    }
+}
