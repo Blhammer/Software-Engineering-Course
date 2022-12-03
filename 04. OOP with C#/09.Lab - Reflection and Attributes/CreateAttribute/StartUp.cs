7ed96e5289257e4bb28f767e51d21cc291fefe54
Blhammer <emo.2012012@gmail.com>
2022-12-03 22:16:16 +0200

    Add solution to C# OOP course

---
+++ 04. OOP with C#/09.Lab - Reflection and Attributes/CreateAttribute/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Bob")]
+    public class StartUp
+    {
+        [Author("Peter")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
