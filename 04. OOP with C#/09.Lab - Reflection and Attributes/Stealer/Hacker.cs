7e69491bcb7741a391ccd680b37211fbd8248229
Blhammer <emo.2012012@gmail.com>
2022-12-04 23:34:54 +0200

    Add solution to C# OOP course

---
+++ 04. OOP with C#/09.Lab - Reflection and Attributes/Stealer/Hacker.cs
@@ -0,0 +1,22 @@
+﻿namespace Stealer
+{
+    public class Hacker
+    {
+        public string username = "bobStealer123";
+        private string password = "passwordSuperPower456#";
+
+        public string Password
+        {
+            get => this.password;
+            set => this.password = value;
+        }
+
+        private int Id { get; set; }
+
+        public double BankAccountBalance { get; private set; }
+
+        public void DownloadAllBankAccountsInTheWorld()
+        {
+        }
+    }
+}
