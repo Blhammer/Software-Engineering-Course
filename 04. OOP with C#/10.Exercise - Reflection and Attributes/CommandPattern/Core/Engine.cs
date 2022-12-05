cc35127036509706b40c14a93a71f913e383c66d
Blhammer <emo.2012012@gmail.com>
2022-12-05 16:09:13 +0200

    Add solution to C# OOP course

---
+++ 04. OOP with C#/10.Exercise - Reflection and Attributes/CommandPattern/Core/Engine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter _command;
+
+        public Engine(ICommandInterpreter command)
+        {
+            _command = command;
+        }
+
+        public void Run()
+        {
+            string engineCondition = string.Empty;
+
+            while (true)
+            {
+                engineCondition = _command.Read(Console.ReadLine());
+
+                if (engineCondition == null) break;
+
+                Console.WriteLine(engineCondition);
+            }
+        }
+    }
+}
