088f70a42882919a735ec37919d5274bbe3feea6
Blhammer <emo.2012012@gmail.com>
2022-12-12 14:31:03 +0200

    Add solution to C# OOP course

---
+++ 04. OOP with C#/11.Exceptions Handling/Fixing/Fixing.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Fixing
+{
+    class Fixing
+    {
+        static void Main(string[] args)
+        {
+            string[] weekdays = new string[7];
+
+            weekdays[0] = "Monday";
+            weekdays[1] = "Tuesday";
+            weekdays[2] = "Wednesday";
+            weekdays[3] = "Thursday";
+            weekdays[4] = "Friday";
+            weekdays[5] = "Saturday";
+            weekdays[6] = "Sunday";
+
+            for (int i = 0; i <= 7; i++)
+            {
+                try
+                {
+                    Console.WriteLine(weekdays[i]);
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
