4ce87b6937707152cf6573dfc054d4c3c0465c2d
Blhammer <emo.2012012@gmail.com>
2023-01-11 23:56:25 +0200

    Added a solution to the C# Advanced course

---
+++ 03. Advanced with C#/Lab/5. Lab - Sets and Dictionaries/06.ParkingLot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parking = new HashSet<string>();
+
+            string input = string.Empty;
+
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] commands = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string command = commands[0];
+                string move = commands[1];
+
+                if (command == "in")
+                {
+                    parking.Add(move);
+                }
+                else
+                {
+                    parking.Remove(move);
+                }
+            }
+
+            if (parking.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty!");
+            }
+
+            foreach (var item in parking)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
